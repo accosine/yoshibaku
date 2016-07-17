9121fef93874c9854a6aaf91d7a6be531b18a6e7
Simon Bugert <simon.bugert@googlemail.com>
2016-07-17 20:20:30 +0000

    WIP

--- src/js/states/alpha/create.js
+++ src/js/states/alpha/create.js
@@ -4,13 +4,15 @@ const COLUMNS = 7;
 
 function create(game) {
   game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  // Style stage
   this.stage.backgroundColor = '#4488AA';
-
-
   const box = minion.drawRect(this, 0, 0, 525, 525, 0xFFFFFF, 1);
   this.field = this.add.sprite(50, 50, box.generateTexture());
+
+  this.emojis = game.add.group();
   for (let i = 0; i < COLUMNS; i++) {
-    this.emojis.push(this.game.add.sprite(50 + i * 75, 50, 'emoji'));
+    this.emojis.create(50 + i * 75, 50, 'emoji');
   }
 
   game.physics.enable(this.emojis, Phaser.Physics.ARCADE);
@@ -19,6 +21,12 @@ function create(game) {
     emoji.body.bounce.y = 0.1;
     emoji.body.gravity.setTo(0, 100);
   });
+
+  this.rect = game.add.sprite(50, 150, null);
+  game.physics.enable(this.rect, Phaser.Physics.ARCADE);
+  this.rect.body.setSize(150, 5, 0, 0); // set the size of the rectangle
+
+
 }
 
 module.exports = create;
--- src/js/states/alpha/update.js
+++ src/js/states/alpha/update.js
@@ -1,4 +1,11 @@
-function update() {}
+function update(game) {
+  game.debug.body(this.emojis);
+  game.debug.body(this.rect);
+  game.physics.arcade.overlap(this.rect, this.emojis, function(r, s) {
+    console.log(r, s);
+    // overlap code here, with r and s corresponding to the rect and sprite objects that have overlapped
+  });
+}
 
 module.exports = update;
 
